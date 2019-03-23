eca3a82c3a81ed0d9ffb67b4828949f4569676a9
AjayB <ajyban@gmail.com>
2019-03-23 22:53:31 +0530

    Added chat client interface

---
+++ src/app/submit/submit.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
+
+@Component({
+  selector: 'app-submit',
+  templateUrl: './submit.component.html',
+  styleUrls: ['./submit.component.css']
+})
+export class SubmitComponent implements OnInit {
+  private socket;
+  constructor() {
+    this.socket = io('http://localhost:3000');
+   }
+  userMsg;
+  messages = [];
+  onSubmit(event){
+    event.preventDefault();
+    this.socket.emit('chat message', this.userMsg);
+    this.userMsg = '';
+
+  }
+  ngOnInit() {    
+    this.socket.on('chat message', (msg)=> {
+      this.messages.push(msg);        
+    });
+  }
+
+}
